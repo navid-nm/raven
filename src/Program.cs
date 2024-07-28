d64830dd4fd54ab82e2afea00135daeccd001cfb
Navid M <navidmomtahen@gmail.com>
2024-07-28 20:57:38 +0100

    ver

--- src/Program.cs
+++ src/Program.cs
@@ -56,7 +56,7 @@ else if (args.Length == 1)
 {
     if (args[0] == "--version")
     {
-        Console.WriteLine("0.0.4");
+        Console.WriteLine("0.0.5");
         return;
     }
     if (args[0] == "--api")
